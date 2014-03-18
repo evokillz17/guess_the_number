e1643b2760471b827918a65808c161ce1ba33113
Evokillz17 <eocampo0808@icloud.com>
2014-03-18 18:06:54 +0000

    check guess a secret number and returned a message

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,3 +1,11 @@
 puts "Welcome to guess the number!"
-secret_number=rand(15)
-puts secret_number
+secret_number = rand(15)
+
+puts "guess a number between 0-14;"
+guess = gets.chomp.to_i
+
+if secret_number == guess
+  puts "u got it correct number is #{secret_number}"
+else
+  puts "wrong the number that you guessed was #{guess}.the number was #{secret_number} changing number"
+end
